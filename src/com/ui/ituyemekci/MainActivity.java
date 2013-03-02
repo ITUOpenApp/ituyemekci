9252310afac8e821731b6c7fd45bba53512ca442
Mert Salık <mertsalik.itu@gmail.com>
2013-03-03 00:46:51 +0200

    Basit sentaks düzenlemesi

--- src/com/ui/ituyemekci/MainActivity.java
+++ src/com/ui/ituyemekci/MainActivity.java
@@ -1,7 +1,5 @@
 package com.ui.ituyemekci;
 
-import java.net.URL;
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
@@ -15,41 +13,26 @@ import android.util.Log;
 import android.view.Menu;
 import android.widget.TextView;
 
-
-
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		Log.v("kod","test");
-		//setContentView(R.layout.activity_main);
+		Log.v("kod", "test");
 
-		String title="...";
-		
-	    // Create the text view
-	    TextView textView = new TextView(this);
-	    textView.setTextSize(40);
-	    
-	    
+		// Create the text view
+		TextView textView = new TextView(this);
+		textView.setTextSize(40);
 
-
-	    new ParseTask().execute("http://www.sks.itu.edu.tr/");
-		
-
-
-	    // Set the text view as the activity layout
-	    setContentView(textView);
-		
-		
+		new ParseTask().execute("http://www.sks.itu.edu.tr/");
+		// Set the text view as the activity layout
+		setContentView(textView);
 	}
 
-	protected void onResume()
-	{
-	   super.onResume();
-	   
+	protected void onResume() {
+		super.onResume();
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -57,44 +40,28 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
-	
-	
 	public void sendMessage(String message) {
-	    Intent intent = new Intent(this, DisplayMenuActivity.class);
-	    
-	    intent.putExtra("menu_content", message);
-	    startActivity(intent);
+		Intent intent = new Intent(this, DisplayMenuActivity.class);
+		intent.putExtra("menu_content", message);
+		startActivity(intent);
 	}
-	
-	
+
 	private class ParseTask extends AsyncTask<String, Void, String> {
 		protected String doInBackground(String... params) {
-			//int count = urls.length;
-			String url=params[0];
-			String menu_content="baðlanamadý..";
+			String menu_content = "baðlanamadý..";
 			try {
-				
-				Document doc = Jsoup.connect("http://www.sks.itu.edu.tr/").timeout(0).get();
-				menu_content = doc.select("div#rightcolumn  td.mod_events_latest_first").text() ;
-				//textView.setText(title);
-				} catch(Exception e) {
-					Log.v("kod",e.toString());
-				}
-
+				Document doc = Jsoup.connect("http://www.sks.itu.edu.tr/")
+						.timeout(0).get();
+				menu_content = doc.select(
+						"div#rightcolumn  td.mod_events_latest_first").text();
+			} catch (Exception e) {
+				Log.v("kod", e.toString());
+			}
 			return menu_content;
-	     }
-
-		   protected void onPostExecute(String result) {
-		         sendMessage(result);
-		     }
-
-		
-
 		}
 
-
-	
-	
-	
-	
+		protected void onPostExecute(String result) {
+			sendMessage(result);
+		}
+	}
 }
