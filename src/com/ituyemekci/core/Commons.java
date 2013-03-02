177873044807dd1dfed16ab4d19a142f9061423e
Mert Salık <mertsalik.itu@gmail.com>
2013-03-03 01:11:10 +0200

    Seperation of concerns

---
+++ src/com/ituyemekci/core/Commons.java
@@ -0,0 +1,10 @@
+package com.ituyemekci.core;
+
+public final class Commons {
+	public static String ConnectionError = "Baðlantý problemi.";
+	public static String DemandingConnection = "Baðlanýlýyor.";
+	public static String SKSUrl = "http://www.sks.itu.edu.tr/";
+	public static String JsoupQuery = "div#rightcolumn  td.mod_events_latest_first";
+	public static String OnCreateLogMessage = "Created Application.";
+	
+}
